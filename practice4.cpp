f241c708f7fc7b119f8b1259aed6aeb5d3d14326
MorningCoder <lycmail58@163.com>
2015-04-13 17:19:40 +0800

    第四个有问题

--- practice4.cpp
+++ practice4.cpp
@@ -1,47 +1,101 @@
 #include <iostream>
 #include <utility>
 #include <random>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-void Merge(int lhs[],int rhs[],int m,int n,int result[])
+void QuickSort(int arr[],int begin,int end)
 {
-	int k = 0;
-    int l = 0,r = 0;
-    //开始合并
-    while(l < m && r < n)
+	if(begin > end)
+        return;
+    //设置轴元素值
+    int pivot = arr[begin];
+    //设置两方向计数
+    int left = begin;
+    int right = end;
+    //扫描一遍数组，将轴元素放至正确位置
+    while(left < right)
     {
-        if(lhs[l] > rhs[r])
-        	result[k++] = lhs[l++];
-        else
-            result[k++] = rhs[r++];
+        //先将right向左移动，找到第一个比轴元素小的元素
+        while(left < right && arr[right] >= pivot)
+            --right;
+        //如果找到则直接将该元素赋值给arr[low]因为arr[low]值被pivot存储
+        if(left < right)
+            arr[left] = arr[right];
+        //再将low向上移动，找到第一个比轴元素大的元素
+        while(left < right && arr[left] <= pivot)
+            ++left;
+        //赋值
+        if(left < right)
+            arr[right] = arr[left];
     }
-    while(l < m)
-        result[k++] = lhs[l++];
-    while(r < n)
-        result[k++] = rhs[r++];
+    //给轴元素赋值
+    arr[left] = pivot;
+    //此时left和right已经相等
+    //递归左右两部分
+    QuickSort(arr,begin,left - 1);
+    QuickSort(arr,right + 1,end);
+}
+
+int Find(int lhs[],int m,int rhs[],int n,int index)
+{
+	int l_begin = 0,r_begin = 0;
+	int l_end = m,r_end = n;
+	int l_mid = (l_begin + l_end)/2;
+	int r_mid = (r_begin + r_end)/2;
+
+	while(l_begin < l_end && r_begin < r_end)
+	{
+		l_mid = (l_end + l_begin)/2;
+		r_mid = (r_end + r_begin)/2;
+		if(lhs[l_mid] > rhs[r_mid])
+		{
+			if(index < l_mid + r_mid)
+				r_end = r_mid;
+			else
+				l_begin = l_mid;
+		}
+		else
+		{
+			if(index < l_mid + r_mid)
+				l_end = l_mid;
+			else
+				r_begin = r_mid;
+		}
+	}
+	if(l_begin == l_end)
+		return rhs[r_begin + index];
+	else if(r_begin == r_end)
+		return lhs[l_begin + index];
+	else
+		return -1;
 }
 
 int main(int argc,char** argv)
 {
-	if(argc < 5)
-	{
-		std::cout << "usage: <m> <n> <k>";
-		exit(1);
-	}
-	default_random_engine engine;
-    uniform_int_distribution<int> u(1,1000);
-
-	int m = std::stoi(argv[1]);
-	int n = std::stoi(argv[2]);
-	int k = std::stoi(argv[3]);
-
+	const int m = stoi(argv[1]);
+	const int n = stoi(argv[2]);
+	const int index = stoi(argv[3]);
+	default_random_engine engine1(32767);
+	default_random_engine engine2(65535);
+	uniform_int_distribution<unsigned> u(1,1000);
+	
 	int* lhs = new int[m];
-	for(int i = 0;i < m;i++)
-		lhs[i] = u(engine);
 	int* rhs = new int[n];
+
+	for(int i = 0;i < m;i++)
+	{
+		lhs[i] = u(engine1);
+		cout << lhs[i] << endl;
+	}
 	for(int i = 0;i < n;i++)
-		rhs[i] = u(engine);
-	int* result = new int[17];
-	Merge(lhs,rhs,10,7,result);
-	std::cout << 
+	{
+		rhs[i] = u(engine2);
+		cout << rhs[i] << endl;
+	}
+	QuickSort(lhs,0,m - 1);
+	QuickSort(rhs,0,n - 1);
+	cout << Find(lhs,m,rhs,n,index) << endl;
 	return 0;
 }
